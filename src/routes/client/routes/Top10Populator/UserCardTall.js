90763bfd5b33cb2ffe68b8128b0186c8513f4160
Achilles Imperial <contact@achillesimperial.com>
2019-08-23 17:58:14 -0400

    improved card sizing

--- src/routes/client/routes/Top10Populator/UserCardTall.js
+++ src/routes/client/routes/Top10Populator/UserCardTall.js
@@ -10,18 +10,17 @@ class UserCardTall extends Component {
                 style={{
                     boxSizing: 'content-box',
                     width: 240, height: '100%',
-                    margin: 18,
-                    userSelect: 'none',
+                    userSelect: 'none',                
                     ...this.props.style
                     }}>
-                <div style={{ flex: 1,}}>
+                <div>
                     <img draggable="false" style={{width: '100%'}} src={`${images[0]}`} alt='user'/>
                 </div>
-                <div style={{ flex: 1, padding: 18,}}>
+                <div style={{ flex: 2, padding: 18,}}>
                     <p style={{fontSize: '18px', fontWeight: 'bold', color: 'rgba(61, 61, 61, 0.99)', }} >{firstName} {lastName}, {age}</p>
                     <p style={{fontSize: '12px', fontWeight: 'regular', color: 'rgba(61, 61, 61, 0.99)', }} >{location}</p>
                     <p style={{fontSize: '15px', fontWeight: 'regular', color: 'rgba(61, 61, 61, 0.66)', fontStyle: 'italic', textAlign: 'center',  margin: '15px 0px',}} >"{headline}"</p>
-                    <div style={{display: window.innerWidth > 320 ? 'inherit' : 'none'}}>
+                    <div style={{display: window.innerWidth > 321 ? 'inherit' : 'none'}}>
                         <p style={{fontSize: '15px', fontWeight: 'bold', color: 'rgba(61, 61, 61, 0.99)', textDecoration: 'underline' }} >I'm About:</p>
                         <p style={{fontSize: '15px', fontWeight: 'regular', color: 'rgba(61, 61, 61, 0.66)', }} >{about}</p>
                     </div>
